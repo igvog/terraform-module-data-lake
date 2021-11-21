9db0f53e1dbf8689be6860c95794a00154a1507d
Marat Okassov <marat@qcloudy.io>
2021-11-21 19:53:49 +0600

    Refactore glue_job

--- glue_job.tf
+++ glue_job.tf
@@ -1,59 +1,65 @@
 resource "aws_glue_job" "glue_job" {
-    count = var.glue_job_enable ? 1 : 0
+  count = var.glue_job_enable ? 1 : 0
 
-    name     = "${lower(var.environment)}-${lower(var.project)}-${lower(var.name)}"
-    role_arn = var.glue_job.role_arn
+  name     = "${lower(var.environment)}-${lower(var.project)}-${lower(var.name)}"
+  role_arn = var.glue_job.role_arn
 
-    description            = var.glue_job.description
-    connections            = var.glue_job.connections
-    default_arguments      = var.glue_job.default_arguments
-    glue_version           = var.glue_job.version != null ? var.glue_job.version : "2.0"
-    max_capacity           = var.glue_job.version != null && var.glue_job.version == "1.0" ? var.glue_job.max_capacity: null
-    max_retries            = var.glue_job.max_retries != null ? var.glue_job.max_retries : 2
-    timeout                = var.glue_job.timeout != null ? var.glue_job.timeout : 60
-    security_configuration = var.glue_job.security_configuration
-    worker_type            = var.glue_job.worker_type != null ? var.glue_job.worker_type : "Standard"
-    number_of_workers      = var.glue_job.number_of_workers != null ? var.glue_job.number_of_workers : 2
+  description            = var.glue_job.description
+  connections            = var.glue_job.connections
+  default_arguments      = var.glue_job.default_arguments
+  glue_version           = var.glue_job.version != null ? var.glue_job.version : "2.0"
+  max_capacity           = var.glue_job.version != null && var.glue_job.version == "1.0" ? var.glue_job.max_capacity : null
+  max_retries            = var.glue_job.max_retries != null ? var.glue_job.max_retries : 2
+  timeout                = var.glue_job.timeout != null ? var.glue_job.timeout : 60
+  security_configuration = var.glue_job.security_configuration
+  worker_type            = var.glue_job.worker_type != null ? var.glue_job.worker_type : "Standard"
+  number_of_workers      = var.glue_job.number_of_workers != null ? var.glue_job.number_of_workers : 2
 
-    dynamic "command" {
-        iterator = command
-        for_each = var.glue_job.command
+  dynamic "command" {
+    iterator = command
+    for_each = var.glue_job.command != null ? var.glue_job.command : [
+      {
+        name            = "glueetl",
+        script_location = "s3://${var.glue_job_bucket}/${var.glue_job_bucket_folder}${var.name}.py",
+        python_version  = "3"
+      }
+    ]
 
-        content {
-            script_location = lookup(command.value, "script_location", null)
-            name            = lookup(command.value, "name", null)
-            python_version  = lookup(command.value, "python_version", null)
-        }
+    content {
+      script_location = lookup(command.value, "script_location", null)
+      name            = lookup(command.value, "name", null)
+      python_version  = lookup(command.value, "python_version", null)
     }
+  }
 
-    dynamic "execution_property" {
-        iterator = execution_property
-        for_each = var.glue_job.execution_property != null ? var.glue_job.execution_property : []
+  dynamic "execution_property" {
+    iterator = execution_property
+    for_each = var.glue_job.execution_property != null ? var.glue_job.execution_property : []
 
-        content {
-            max_concurrent_runs = lookup(execution_property.value, "max_concurrent_runs", 1)
-        }
+    content {
+      max_concurrent_runs = lookup(execution_property.value, "max_concurrent_runs", 1)
     }
+  }
 
-    dynamic "notification_property" {
-        iterator = notification_property
-        for_each = var.glue_job.notification_property != null ? var.glue_job.notification_property: []
+  dynamic "notification_property" {
+    iterator = notification_property
+    for_each = var.glue_job.notification_property != null ? var.glue_job.notification_property : []
 
-        content {
-            notify_delay_after = lookup(notification_property.value, "notify_delay_after", null)
-        }
+    content {
+      notify_delay_after = lookup(notification_property.value, "notify_delay_after", null)
     }
+  }
 
-    tags = {
-        Managed_By = "terraform"
-        Env = var.environment
-        Project = var.project
-        App = "glue"
-        Name = "${lower(var.environment)}-${lower(var.project)}-${lower(var.name)}"
-    }
+  tags = {
+    Managed_By = "terraform"
+    Env        = var.environment
+    Project    = var.project
+    App        = "glue"
+    Name       = "${lower(var.environment)}-${lower(var.project)}-${lower(var.name)}"
+  }
 
-    lifecycle {
-        create_before_destroy = true
-        ignore_changes        = []
-    }
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = []
+  }
 }
--- variables.tf
+++ variables.tf
@@ -3,40 +3,49 @@
 #---------------------------------------------------
 
 variable "environment" {
-    description = "Environment"
+  description = "Environment"
 }
 
 variable "project" {
-    description = "Global Project name"
+  description = "Global Project name"
 }
 
 variable "name" {
-    description = "Project name"
+  description = "Project name"
 }
 
 variable "glue_job_enable" {
-    description = "Enable Glue job creation"
-    type = bool
+  description = "Enable Glue job creation"
+  type        = bool
+}
+
+variable "glue_job_bucket" {
+  description = "S3 bucket name for glue job scripts"
+}
+
+variable "glue_job_bucket_folder" {
+  description = "Folder in S3 bucket"
+  default = null
 }
 
 variable "glue_job" {
-    description = "Glue job"
-    type = object({
-        description = string
-        version = optional(string)
-        timeout = optional(number)
-        command = list(map(string))
-        worker_type = optional(string)
-        number_of_workers = optional(number)
-        max_capacity = optional(number)
-        max_retries = optional(number)
-        role_arn = string
-        additional_libs = optional(list(string))
-        connections = optional(list(string))
-        execution_property = optional(list(map(string)))
-        notification_property = optional(list(map(string)))
-        security_configuration = optional(string)
-        default_arguments = optional(map(string))
-        schedule = optional(list(string))
-    })
+  description = "Glue job"
+  type = object({
+    role_arn               = string
+    command                = list(map(string))
+    description            = optional(string)
+    version                = optional(string)
+    timeout                = optional(number)
+    worker_type            = optional(string)
+    number_of_workers      = optional(number)
+    max_capacity           = optional(number)
+    max_retries            = optional(number)
+    additional_libs        = optional(list(string))
+    connections            = optional(list(string))
+    execution_property     = optional(list(map(string)))
+    notification_property  = optional(list(map(string)))
+    security_configuration = optional(string)
+    default_arguments      = optional(map(string))
+    schedule               = optional(list(string))
+  })
 }
