7759663a44dd2e3b2db52abc26aeae32bf59435a
Bakhytzhan Sambayev <62016400+bahabaii@users.noreply.github.com>
2022-08-04 10:15:25 +0600

    add new extra-py-files for glue-job

--- glue_job.tf
+++ glue_job.tf
@@ -30,7 +30,7 @@ resource "aws_glue_job" "glue_job" {
     "--enable-continuous-log-filter"     = "true"
     "--enable-metrics"                   = "true"
     "--TempDir"                          = "s3://aws-glue-assets-313555887466-eu-north-1/temporary/" 
-    "--extra-py-files"                   = "s3://technodom-prod-data-lake-s3-glue-libs/delta-core_2.12-1.0.1.jar"
+    "--extra-py-files"                   = "s3://technodom-prod-data-lake-s3-glue-libs/delta-core_2.12-1.0.1.jar,s3://technodom-prod-data-lake-s3-glue-libs/technolibs.zip"
     "--extra-jars"                       = "s3://technodom-prod-data-lake-s3-glue-libs/delta-core_2.12-1.0.1.jar"
     "--spark-event-logs-path"            = "s3://aws-glue-assets-313555887466-eu-north-1/sparkHistoryLogs/"
     "--user-jars-first"                  = "true"
