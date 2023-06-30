6480aef4d21056d6de2e71e4d9a8ae80493d215a
Samat Kurmanov <igvog88@gmail.com>
2023-06-30 11:34:03 +0600

    function name

--- lambda.tf
+++ lambda.tf
@@ -393,7 +393,7 @@ resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
 resource "aws_lambda_permission" "test" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.test_lambda.function_name}"
+  function_name = local.full_name #"${aws_lambda_function.test_lambda.function_name}"
   principal = "s3.amazonaws.com"
   source_arn = "arn:aws:s3:::${aws_s3_bucket.bucket.id}"
 }
