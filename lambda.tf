5e984750c3fafc4035a152ec5f5f9db49f7c76db
Samat Kurmanov <igvog88@gmail.com>
2023-06-30 11:03:57 +0600

    test trigger

--- lambda.tf
+++ lambda.tf
@@ -377,3 +377,23 @@ resource "aws_lambda_function_url" "lambda_function_uri" {
   #  }
   #}
 }
+
+##################
+# Adding S3 bucket as trigger to my lambda and giving the permissions
+##################
+resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+  lambda_function {
+  lambda_function_arn = "${aws_lambda_function.test_lambda.arn}"
+  events              = ["s3:ObjectCreated:*"]
+  filter_prefix       = "file-prefix"
+  filter_suffix       = "file-extension"
+  }
+}
+resource "aws_lambda_permission" "test" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.test_lambda.function_name}"
+  principal = "s3.amazonaws.com"
+  source_arn = "arn:aws:s3:::${aws_s3_bucket.bucket.id}"
+}
